6725730f4bbfaed7d53d52d1f44efb2850b6044d
Ecaterina Baba <keepoutzone1@gmail.com>
2024-01-11 15:10:14 +0100

    install dev dependencies, restructure

---
+++ src/app/App.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import { Autocomplete } from '../features/Autocomplete';
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>App</h1>
+      <Autocomplete />
+    </div>
+  );
+}
+
+export default App;
